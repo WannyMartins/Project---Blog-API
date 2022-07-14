f51cbceb2ceb5684d7f78541b8f2f524716a477f
Wanny Martins <wcatery@gmail.com>
2022-07-14 16:02:12 -0300

    feat(Category): criando novo model para categorias

---
+++ src/database/models/category.js
@@ -0,0 +1,17 @@
+const createCategory = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', 
+  {
+    id: { 
+      type: DataTypes.INTEGER, allowNull: false, autoIncrement: true,primaryKey: true
+    },
+    name: DataTypes.STRING,
+  }, 
+  {
+    tableName: 'Categories',
+    timestamps: false,
+  });
+
+return Category;
+};
+
+module.exports = createCategory;
